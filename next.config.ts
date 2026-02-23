e8d182031d8bd4f66b9d793413dff686f952a15e
nnnnssssh <syifanursilah68@gmail.com>
2026-02-23 11:20:21 +0700

    regis

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://payroll.politekniklp3i-tasikmalaya.ac.id/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
